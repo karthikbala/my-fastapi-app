ec63d5c4558a4541846ef22f8c0e354af0439f42
Karthik Bala <karthik@mieone.com>
2023-12-06 16:16:30 +0530

    Add static file serving and favicon endpoint

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/favicon.ico")
+def get_favicon():
+    return FileResponse("static/favicon.ico")
